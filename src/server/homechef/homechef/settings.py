27b39e75e82a5916665bff33a6714af6f91e7dbf
oet247 <teodorzlatanov3v2@gmail.com>
2023-10-10 21:43:56 +0300

    structure fix

--- src/server/homechef/homechef/settings.py
+++ src/server/homechef/homechef/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&%n&_wbqxp1t!&^8f$1lh^l7)*^kbo$6c$iz_y)1_gjd^-1$86'
+SECRET_KEY = 'django-insecure-he^&*7qi++ycrty*%#1mpd+x!pgj++#v9_8)cz%t3_!yeolnx$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api.apps.ApiConfig',
-    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -77,13 +76,9 @@ WSGI_APPLICATION = 'homechef.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "homechef",
-        "USER": "postgres",
-        "PASSWORD": "oet",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
--- src/server/homechef/homechef/urls.py
+++ src/server/homechef/homechef/urls.py
@@ -15,11 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-
-import api.urls
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(api.urls.urlpatterns))
 ]
