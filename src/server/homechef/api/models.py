c952b85571bdb8a7c336acabdbb2bc7d44ce93b3
oet247 <teodorzlatanov3v2@gmail.com>
2023-10-10 22:10:32 +0300

    test

--- src/server/homechef/api/models.py
+++ src/server/homechef/api/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Uchenik(models.Model):
+    uspeh = models.CharField(max_length=100)
+    creativnost = models.CharField(max_length=100)
+    potencial = models.CharField(max_length=100)
--- src/server/homechef/homechef/settings.py
+++ src/server/homechef/homechef/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'homechef.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "homechef",
+        "USER": "postgres",
+        "PASSWORD": "oet",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
